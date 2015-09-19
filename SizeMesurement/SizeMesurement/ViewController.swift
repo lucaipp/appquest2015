aa3034f992b54aae028e984415623e8207f5ef47
lucaipp <lucaippolito@hotmail.ch>
2015-09-19 21:16:58 +0200

    worked

--- SizeMesurement/SizeMesurement/ViewController.swift
+++ SizeMesurement/SizeMesurement/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//    @IBOutlet weak var txtDistance: UITextField!
+//    @IBOutlet weak var txtHeight: UITextField!
+//    @IBOutlet weak var swMode: UISwitch!
+//    @IBOutlet weak var btnTakeMesurement: UIButton!
+//    @IBOutlet weak var btnLog: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        swMode.setOn(false, animated: false)
+        
+        enableGuiElements(false)
+//        btnLog.enabled = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func enableGuiElements(enabled: Bool) {
+//        txtHeight.enabled = enabled
+//        btnTakeMesurement.enabled = enabled
     }
-
-
+    
+    @IBAction func changeGuiState(sender: AnyObject) {
+//        enableGuiElements(swMode.on)
+    }
 }
 
