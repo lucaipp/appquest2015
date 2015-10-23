7d92b37d4dc4a0ade7589a469a5b357fcf8ed5ea
lucaipp <lucaippolito@hotmail.ch>
2015-10-23 10:14:06 +0200

    redo

--- Schatzkarte/Schatzkarte/MapViewController.swift
+++ Schatzkarte/Schatzkarte/MapViewController.swift
@@ -2,15 +2,20 @@ import UIKit
 import CoreLocation
 
 class MapViewController: UIViewController {
-    let kAccessToken = "<Access-Token aus der Aufgabenstellung>"
+    let kAccessToken = "sk.eyJ1IjoidG9uaXN1dGVyIiwiYSI6ImNpZmptbnhxYTAxMGR0ZWx4ZjFhejdkMzEifQ.4HxuC8B4MW_slik23J9NqQ"
     let kMapID = "tonisuter.cife1ku4000gmtaknuv49tvwc"
     let kHsrCoordinate = CLLocationCoordinate2DMake(47.223252, 8.817011)
+    var kMapView: RMMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         RMConfiguration.sharedInstance().accessToken = kAccessToken
+        kMapView = RMMapView(frame: self.view.bounds, andTilesource: RMMapboxSource(mapID: "mapbox.run-bike-hike")!)
+        self.view.addSubview(kMapView)
         
+        kMapView.zoom = 15
+        kMapView.centerCoordinate = kHsrCoordinate
         // Hier sollte der locationManager konfiguriert und gestartet werden.
         // Zusätzlich muss der MapViewController das CLLocationManagerDelegate-Protokoll
         // implementieren, damit die Location-Updates empfangen werden können.
