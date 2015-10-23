a5a05e4f9eba565fa20b50674853f70ca0d5e0a0
lucaipp <lucaippolito@hotmail.ch>
2015-10-23 12:03:30 +0200

    commit

--- Schatzkarte/Schatzkarte/MapViewController.swift
+++ Schatzkarte/Schatzkarte/MapViewController.swift
@@ -1,31 +1,69 @@
 import UIKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
     let kAccessToken = "sk.eyJ1IjoidG9uaXN1dGVyIiwiYSI6ImNpZmptbnhxYTAxMGR0ZWx4ZjFhejdkMzEifQ.4HxuC8B4MW_slik23J9NqQ"
     let kMapID = "tonisuter.cife1ku4000gmtaknuv49tvwc"
     let kHsrCoordinate = CLLocationCoordinate2DMake(47.223252, 8.817011)
     var kMapView: RMMapView!
+    let kLocationManager = CLLocationManager()
+    var kCurrentLocationMarker: RMPointAnnotation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         RMConfiguration.sharedInstance().accessToken = kAccessToken
-        kMapView = RMMapView(frame: self.view.bounds, andTilesource: RMMapboxSource(mapID: "mapbox.run-bike-hike")!)
+        kMapView = RMMapView(frame: self.view.bounds, andTilesource: RMMapboxSource(mapID: kMapID)!)
         self.view.addSubview(kMapView)
         
-        kMapView.zoom = 15
-        kMapView.centerCoordinate = kHsrCoordinate
-        // Hier sollte der locationManager konfiguriert und gestartet werden.
-        // Zusätzlich muss der MapViewController das CLLocationManagerDelegate-Protokoll
-        // implementieren, damit die Location-Updates empfangen werden können.
+        kCurrentLocationMarker = RMPointAnnotation()
+        kCurrentLocationMarker.mapView = kMapView
+        kMapView.addAnnotation(kCurrentLocationMarker)
+        
+        kLocationManager.delegate = self
+        if CLLocationManager.locationServicesEnabled() {
+            kLocationManager.startUpdatingLocation()
+        }
+        
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            kLocationManager.requestAlwaysAuthorization()
+        }
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-
-        // Hier soll die MapView konfiguriert werden.
-        // Zum Beispiel kann man den Zoom-Level oder 
-        // die center coordinate setzen.
+        
+        kMapView.zoom = 16
+        
+        //Laden der Punkte
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+            kLocationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locationArray = locations as NSArray
+        let locationObj = locationArray.lastObject as! CLLocation
+        let coord = locationObj.coordinate
+        var refresh = false
+        
+        if kCurrentLocationMarker.coordinate.longitude == 0.0 && kCurrentLocationMarker.coordinate.latitude == 0.0 {
+            refresh = true
+        }
+        
+        kCurrentLocationMarker.coordinate = CLLocationCoordinate2DMake(coord.latitude, coord.longitude)
+        
+        if refresh {
+            locate(self)
+        }
+    }
+    
+    @IBAction func locate(sender: AnyObject) {
+        kMapView.centerCoordinate = kCurrentLocationMarker.coordinate
     }
 }
